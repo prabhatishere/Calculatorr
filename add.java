9fdda22d6a1be44681bbd3728fb455dc608268a0
prabhatishere <prabhatkumar14504@gmail.com>
2025-01-17 22:26:16 +0530

    Doing something

--- add.java
+++ add.java
@@ -1,11 +1,10 @@
 import java.util.*;
-public class add{
+public class sub{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
-        int num3 = sc.nextInt();
-        int sum = num1 + num2 + num3;
-        System.out.println("The sum is "+sum);
+        int sum = 0;
+        System.out.println("The Addiion is "+sum);
     }
 }

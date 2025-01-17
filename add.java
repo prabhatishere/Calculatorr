ed6b88b1cb1a4f72a948584446c81eb1ea51b9b5
prabhatishere <prabhatkumar14504@gmail.com>
2025-01-17 14:04:43 +0530

    Program done of Addition

--- add.java
+++ add.java
@@ -1,1 +1,11 @@
-public class
+import java.util.*;
+public class add{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        int sum = num1 + num2 + num3;
+        System.out.println("The sum is "+sum);
+    }
+}
